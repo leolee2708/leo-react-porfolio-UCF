5334d7761f4da6054bd17bf1b84e863f72bdf8e3
leolee2708 <leho.vietquan@gmail.com>
2021-03-02 23:44:44 -0500

    all files created

---
+++ leoreactportfolio/README.md
@@ -0,0 +1,35 @@
+# React Portfolio
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributors](#contributors)
+  * [License](#license)
+  * [Test](#test)
+  * [Questions](#questions)
+  
+  ## Description
+  My own portfolio created using react
+  
+  ## Installation 
+  git fork or git clone
+  
+  ## Usage 
+  none
+  
+  ## Contributors rules
+  NONE
+  
+  ## License
+  MIT
+  
+  ## Test
+  deployed Link
+  ## Questions
+  Contact me:
+  
+  Github:[leolee2708](https://github.com/leolee2708)
+  
+  Email:[leho.vietquan@gmail.com](https://github.com/leolee2708)
+  
---
+++ leoreactportfolio/src/components/Carousel.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import Card from '../components/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+//import links for images
+import profile from "../assets/images/profile.png";
+
+import github from '../assets/images/github.png';
+import linkedin from '../assets/images/linkedin.png';
+import burgerhw from '../assets/images/burgerhw.png';
+// import Col from 'react-bootstrap/Col';
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: 'Eat da burger',
+                    subTitle: 'a fun app to eat some hamburgers',
+                    imgSrc: burgerhw,
+                    link: 'https://lit-island-14787.herokuapp.com/',
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: 'My Github repo',
+                    subTitle: 'My github repo with stared projects',
+                    imgSrc: github,
+                    link: 'https://github.com/leolee2708',
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: 'my profile',
+                    subTitle: 'Click the link below for my resume',
+                    imgSrc: profile,
+                    link: "../assets/images/QuanLeGIT-A-CV.pdf",
+                    selected: false
+                },
+                {
+                    id: 3,
+                    title: 'my linkedin',
+                    subTitle: 'My linkedin profile',
+                    imgSrc: linkedin,
+                    link: 'https://www.linkedin.com/in/quan-le-2054a432/',
+                    selected: false
+                }
+
+
+            ]
+        }
+    }
+
+    handleCardClick = (id, card) => {
+
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
+
+    render() {
+        return(
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
+        );
+    }
+
+}
+
+export default Carousel;
